65c3ba9b4ef8d6878baa6fd6d75a7dd1ecc40c54
AadithyaRao2809 <aadithyarao@gmail.com>
2023-11-06 22:12:04 +0530

    update .gitignore

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+  <svelte:head>
+    <meta name="robots" content="noindex" />
+  </svelte:head>
+  
+  <nav>
+    <a href="/home">HOME</a>
+    <a href="/about">ABOUT</a>
+  </nav>
+  
+<slot/>
+  
+  <style>
+    nav {
+      padding: 1rem;
+      box-shadow: -1px 1px 11px 4px #898989;
+    }
+    a {
+      text-decoration: none;
+      color: gray;
+      margin-right: 1rem;
+    }
+  </style>
